16f8055cfb20250443b31ea9b66010f4ca8dc88f
xdaruis <rozdaruis@gmail.com>
2024-07-05 19:47:00 +0300

    ➕ Add static files dependencies

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from drf_spectacular.views import (
   SpectacularAPIView,
@@ -17,3 +19,6 @@ urlpatterns = [
   ),
   path('api/user/', include('user.urls')),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
