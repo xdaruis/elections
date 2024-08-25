2c24e1f70ae49a86fc65f823e7a61407b946682b
xdaruis <rozdaruis@gmail.com>
2024-08-25 15:33:58 +0300

    🚚 Make a folder for each component

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -3,10 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
-import CustomCard from '../components/ui/CustomCard';
-import CustomInput from '../components/ui/CustomInput';
-import InfoText from '../components/ui/InfoText';
-import { login } from '../features/auth';
+import CustomCard from '../../components/ui/CustomCard';
+import CustomInput from '../../components/ui/CustomInput';
+import InfoText from '../../components/ui/InfoText';
+import { login } from '../../features/auth';
 
 function Login() {
   const [error, setError] = useState('');
